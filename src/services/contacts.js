977c6b9d2fd65aa4ff9e4f861a39e771656aacfa
Ruslan Zahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2021-05-13 22:50:26 +0200

    start hm02-express

---
+++ src/services/contacts.js
@@ -0,0 +1,21 @@
+class contactsService {
+    constructor()
+    getAllContacts() {
+        return {}
+    }
+     getContactById(id) {
+        return {}
+    }
+     createContact(body) {
+        return {}
+    }
+     updateContact(id,body) {
+        return {}
+    }
+     removeContact(id) {
+        return {}
+    }
+
+}
+
+module.exports = contactsService
