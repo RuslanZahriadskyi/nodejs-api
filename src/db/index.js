e25ed55ceae05590db9e7ee9ef6ece19b640345c
Ruslan Zahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2021-05-17 22:12:27 +0200

    start hw03

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,13 @@
-const low = require("lowdb");
-const path = require("path");
-const FileSync = require("lowdb/adapters/FileSync");
+// const low = require("lowdb");
+// const path = require("path");
+// const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync(
-  path.join(__dirname, "..", "..", "model", "contacts.json")
-);
+// const adapter = new FileSync(
+//   path.join(__dirname, "..", "..", "model", "contacts.json")
+// );
 
-const db = low(adapter);
+// const db = low(adapter);
 
-db.defaults({ contacts: [] }).write();
+// db.defaults({ contacts: [] }).write();
 
-module.exports = db;
+// module.exports = db;
